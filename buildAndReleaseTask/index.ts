f358d0f89846c1341c2adb8932378b19dd4eace9
Dan Hyrjak <dan.hyrjak@prospectsoft.com>
2019-09-16 16:54:21 +0100

    placeholder extenstion

---
+++ buildAndReleaseTask/index.ts
@@ -0,0 +1,15 @@
+import tl = require('azure-pipelines-task-lib/task');
+
+(async function() {
+    try {
+        const inputString: string = tl.getInput('samplestring', true);
+        if (inputString == 'bad') {
+            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+            return;
+        }
+        console.log('Hello', inputString);
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+})();
